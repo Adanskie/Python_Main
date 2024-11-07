0d16e9d437681abbfee8d6c3ba1d74ed34fc137a
Adanskie <155322408+Adanskie@users.noreply.github.com>
2024-11-07 15:34:00 +0800

    Practice_1.py

---
+++ Practice_1.py
@@ -0,0 +1,14 @@
+print("Hello World")
+
+numOne = 25
+numTwo = 25
+
+sum = numOne + numTwo
+print("The Result : " + str(sum))
+
+age = 20
+
+if age >= 18:
+  print("Legal age")
+elif age <= 17:
+  print("Not Legal age")
