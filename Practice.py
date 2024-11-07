c2b7ad1a4b97569e5afa8865b20a5f88a5f0a5fb
Adanskie <155322408+Adanskie@users.noreply.github.com>
2024-11-07 15:47:56 +0800

    Practice.py

--- Practice.py
+++ Practice.py
@@ -1,1 +1,12 @@
-# Python_Main
+def check_even_odd(number):
+    if number % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+
+# Input from the user
+num = int(input("Enter a number: "))
+
+# Check and print if the number is even or odd
+result = check_even_odd(num)
+print(f"The number {num} is {result}.")
